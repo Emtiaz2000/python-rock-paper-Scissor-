f9f596278fa250ac5b39afc1caaa0b90f673cf35
Emtiaz2000 <62500633+Emtiaz2000@users.noreply.github.com>
2021-12-19 21:57:06 +0600

    Add files via upload

---
+++ rock_paper_seisssure.py
@@ -0,0 +1,30 @@
+# rock paper scissure game
+
+#importing random
+import random
+computer_pick = random.randint(0,3)#random intiger 
+game_elements=["rock",'paper','scissure']#list
+user_input=input('Select Rock Paper or Scissure ') #user input
+user_input = user_input.lower() #making user input to lowerCase
+
+#Check if the user entered the right word
+if user_input == 'rock' or user_input == 'paper' or user_input == 'scissure':
+    #user entered the entered right word
+    if user_input == game_elements[computer_pick]:
+        print("Draw!")
+        print(game_elements[computer_pick] )
+        print(user_input )
+    elif user_input == "scissure" and game_elements[computer_pick] =='rock':
+        print('OOps you loss!')
+        print(game_elements[computer_pick] )
+    elif user_input == "rock" and game_elements[computer_pick] =='paper':
+        print('OOps you loss!')
+        print(game_elements[computer_pick] )
+    elif user_input == "paper" and game_elements[computer_pick] =='scissure':
+        print('OOps you loss!')
+        print(game_elements[computer_pick] )
+    else:
+        print('You win!')
+        print(game_elements[computer_pick] )
+else:#user entered wrong word
+    print("You have to Choose Rock Paper or Seissure!")
